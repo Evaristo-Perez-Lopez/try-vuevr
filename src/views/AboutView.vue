95375a5537e5a8fa590483a653c9f2f70d8e0a05
Evaristo Perez <perezlopezevaristo07@outlook.com>
2023-05-08 17:54:17 -0500

    Test vr

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,100 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="divisor">
+    <div class="visor">
+      <Tour :scenes="scenes"></Tour>
+    </div>
+    <div class="aux">
+      <img src="@/assets/faces/pano_b.jpg" alt="" srcset="" />
+      <a href="https://mudin.github.io/vue-vr/#/demo-pano">vr</a>
+    </div>
+  </section>
 </template>
+
+<script>
+import { Tour } from "vuejs-vr";
+export default {
+  name: "App",
+  components: { Tour },
+  data: () => ({
+    url: ["../assets/equirectangular.jpg", "../assets/pano.png"],
+    scenes: [
+      {
+        key: "M4wOy2s",
+        panorama: {
+          source: require("@/assets/equirectangular.jpg"),
+          type: "image",
+        },
+        x: 0,
+        y: 0,
+        z: 0,
+        rx: 0,
+        rz: 0,
+        ry: 90,
+        connections: ["eIm7la", "eId9l0", "fK2dL7l"],
+      },
+      {
+        key: "eIm7la",
+        panorama: {
+          source: require("@/assets/faces/pano_l.jpg"),
+          type: "cube",
+        },
+        x: 100,
+        y: 0,
+        z: 200,
+        rx: 0,
+        rz: 0,
+        ry: 0,
+        connections: ["M4wOy2s"],
+      },
+      {
+        key: "eId9l0",
+        panorama: {
+          source: require("@/assets/faces/pano_u.jpg"),
+          type: "cube",
+        },
+        x: 150,
+        y: 0,
+        z: -300,
+        rx: 0,
+        rz: 0,
+        ry: 0,
+        connections: ["M4wOy2s"],
+      },
+      {
+        key: "fK2dL7l",
+        panorama: {
+          source: require("@/assets/faces/pano_d.jpg"),
+          type: "cube",
+        },
+        x: -150,
+        y: 0,
+        z: -300,
+        rx: 0,
+        rz: 0,
+        ry: 0,
+        connections: ["M4wOy2s"],
+      },
+    ],
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+.divisor {
+  display: flex;
+  flex-direction: column-reverse;
+}
+.visor {
+  display: block;
+  height: 500px;
+  width: 100%;
+}
+.aux {
+  height: 250px;
+  width: 100%;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="scene-try">
+    <Scene
+      :pano="scene.pano"
+      :position="scene.position"
+      :rotation="scene.rotation"
+      :links="scene.links"
+    >
+    </Scene>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+// eslint-disable-next-line
+import { Scene } from "vuejs-vr";
 export default {
-  name: "HomeView",
   components: {
-    HelloWorld,
+    Scene,
   },
+  data: () => ({
+    scene: {
+      pano: {
+        source: "/faces/pano_%s.jpg",
+        type: "cube",
+      },
+      position: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+      rotation: {
+        x: 0,
+        y: (135.0 * Math.PI) / 180.0,
+        z: 0,
+      },
+      links: [
+        {
+          sceneId: "scene1",
+          position: {
+            x: 100,
+            y: 0,
+            z: -100,
+          },
+        },
+        {
+          sceneId: "scene2",
+          position: {
+            x: 200,
+            y: 0,
+            z: -20,
+          },
+        },
+      ],
+    },
+  }),
 };
 </script>
+
+<style lang="scss" scoped>
+.scene-try {
+  height: 500px;
+  width: 100%;
+  border: 5px solid burlywood;
+  box-sizing: border-box;
+}
+</style>
